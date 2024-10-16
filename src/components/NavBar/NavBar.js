e48318aced8c4855f9bebb7c9b5f24b99b0e6bcb
Bryan Camilo Pineda <66718960+BryanCPineda@users.noreply.github.com>
2024-10-15 22:20:21 -0300

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,9 +7,6 @@ import './Navbar.css';
 export default function NavBar() {
     return (
         <header className="navbar">
-            <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </div>
             <nav>
                 <ul className="list">
                     <li className="list-item">
